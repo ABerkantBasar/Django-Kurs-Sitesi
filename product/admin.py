b1435a071273cf125f38c0e0026f6a4723186f16
BerkantBasar <b210109027@subu.edu.tr>
2022-11-21 23:10:44 +0300

    Home sayfası eklendi

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from product.models import Category
+from product.models import Category, Course
 
 class CategoryAdmin(admin.ModelAdmin):
     #fields = ['title', 'status'] sadece title ve status alır
     list_display=['title', 'status']
     list_filter=['status']      #filtreleme ypar
 admin.site.register(Category,CategoryAdmin)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display=['title', 'status','price','category','image']
+    list_filter=['status','category']      #filtreleme ypar
+admin.site.register(Course,CourseAdmin)
--- product/models.py
+++ product/models.py
@@ -18,3 +18,32 @@ class Category(models.Model):
     update_at=models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title            #geri dönüş
+
+class Course(models.Model):
+    STATUS=(
+        ('True','Evet'),
+        ('False','Hayir'),
+    )
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)     #relation with category
+    title=models.CharField(max_length=150)
+    description=models.CharField(max_length=255)
+    keywords=models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    price=models.FloatField()
+    detail=models.TextField()              #RichTextUploadingField()
+    status=models.CharField(max_length=10,choices=STATUS)
+
+    #slug=models.SlugField()     #id gibi ama metin 
+    #parent=models.ForeignKey('self',blank=True,null=True,related_name='lesson',on_delete=models.CASCADE)
+
+    crate_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title            #geri dönüş
+    
+    #def get_cat_list(self):
+        k=self.category
+        breadcrumb=["dummy"]
+        while k is not None:
+            breadcrumb.append(k.slug)
+            k=k.parent
