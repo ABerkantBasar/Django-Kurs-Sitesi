bbffd489ce80df99e7427b7036bc3bd441aaa286
BerkantBasar <b210109027@subu.edu.tr>
2022-11-23 23:59:15 +0300

    Resim Galerisi ve image görme

--- product/admin.py
+++ product/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
-from product.models import Category, Course
+from product.models import Category, Course, Images
+
 
 class CategoryAdmin(admin.ModelAdmin):
     #fields = ['title', 'status'] sadece title ve status alır
-    list_display=['title', 'status']
+    readonly_fields =('image_tag',)
+    list_display=['title', 'status','image_tag']
     list_filter=['status']      #filtreleme ypar
 admin.site.register(Category,CategoryAdmin)
 
+class CourseImageInline(admin.TabularInline):
+    model=Images
+    extra=3
+
 class CourseAdmin(admin.ModelAdmin):
-    list_display=['title', 'status','price','category','image']
+    readonly_fields =('image_tag',)
+    list_display=['title', 'status','price','category','image_tag']
     list_filter=['status','category']      #filtreleme ypar
+    inlines=[CourseImageInline]
+    
 admin.site.register(Course,CourseAdmin)
+
+class ImagesAdmin(admin.ModelAdmin):
+    readonly_fields =('image_tag',)
+    list_display=['title', 'course','image_tag']
+    list_filter=['course','title']      #filtreleme ypar
+admin.site.register(Images,ImagesAdmin)
+
+
--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 class Category(models.Model):
     STATUS=(
@@ -18,7 +19,9 @@ class Category(models.Model):
     update_at=models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title            #geri dönüş
-
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    
 class Course(models.Model):
     STATUS=(
         ('True','Evet'),
@@ -41,9 +44,25 @@ class Course(models.Model):
     def __str__(self):
         return self.title            #geri dönüş
     
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+    
     #def get_cat_list(self):
-        k=self.category
-        breadcrumb=["dummy"]
-        while k is not None:
-            breadcrumb.append(k.slug)
-            k=k.parent
+        #k=self.category
+        #breadcrumb=["dummy"]
+        #while k is not None:
+            #breadcrumb.append(k.slug)
+            #k=k.parent
+
+class Images(models.Model):
+    course=models.ForeignKey(Course,on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    image=models.ImageField(blank=True,upload_to='images/')
+    def __str__(self):
+        return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    
+    
